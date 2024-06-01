72066423336c6defa99f22677a17474a2e14b238
Carlos Toledo <carlombo@hotmail.com>
2024-06-01 00:22:55 -0400

    [ADD] Added templates and views for base, index, about, welcome, _footer and _navbar. Also added static image (logo)

--- main/views.py
+++ main/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Pagina de inicio')
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def welcome(request):
+    return render(request, 'welcome.html')
