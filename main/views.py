c34f3f622c22af8ee874c17e0067c8fff68cac89
Carlos Toledo <carlombo@hotmail.com>
2024-06-05 13:44:10 -0400

    [UPDATE] Renamed pasteles.py to pasteles.json and moved it to the new 'data' folder. Updated index view to reflect changes.

--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from main.pasteles import pasteles
+import json
+import os
 
 # Create your views here.
 def index(request):
+    # file_path = 'main/data/pasteles.json'
+    file_path = os.path.join(os.path.dirname(__file__), 'data', 'pasteles.json') # Obtiene el directorio de views.py y combina con 'data' y 'personas.json' para crear la ruta. Esta forma es portable, mantenible y buena práctica
+    with open(file_path, 'r') as file: 
+        pasteles = json.load(file)
     context = {'productos': pasteles}
     return render(request, 'index.html', context)
 
