ad89512384917e78eff478648f4bb3c9a8c1236d
Carlos Toledo <carlombo@hotmail.com>
2024-06-04 21:07:36 -0400

    [ADD] Adding dinamic card

---
+++ main/pasteles.py
@@ -0,0 +1,44 @@
+pasteles = [
+  {
+    "id": 1,
+    "nombre": "Flan Borracho",
+    "descripcion": "Postre a base de pan frio",
+    "imagen_url": "https://mojo.generalmills.com/api/public/content/nVkc4H0aVkCcWbegp426XA_gmi_hi_res_jpeg.jpeg",
+    "precio": 3000
+  },
+  {
+    "id": 2,
+    "nombre": "Flan de Coco",
+    "descripcion": "Delicioso flan con sabor tropical de coco",
+    "imagen_url": "https://imag.bonviveur.com/flan-de-coco.webp",
+    "precio": 3500
+  },
+  {
+    "id": 3,
+    "nombre": "Flan Napolitano",
+    "descripcion": "Clásico flan con un toque de vainilla y caramelo",
+    "imagen_url": "https://www.recetasnestle.com.mx/sites/default/files/srh_recipes/54c6cbcbc6d611e056122d64560cd9c1.jpg",
+    "precio": 3200
+  },
+  {
+    "id": 4,
+    "nombre": "Flan de Queso",
+    "descripcion": "Flan cremoso con queso suave incorporado",
+    "imagen_url": "https://mojo.generalmills.com/api/public/content/2jke2MeMu0u0v0iYEXkYeQ_gmi_hi_res_jpeg.jpeg",
+    "precio": 3400
+  },
+  {
+    "id": 5,
+    "nombre": "Flan de Café",
+    "descripcion": "Para los amantes del café, un flan con intenso sabor",
+    "imagen_url": "https://imag.bonviveur.com/flan-de-cafe.webp",
+    "precio": 3600
+  },
+  {
+    "id": 6,
+    "nombre": "Flan de Chocolate",
+    "descripcion": "Flan con rico chocolate derretido en su mezcla",
+    "imagen_url": "https://images.aws.nestle.recipes/resized/2020_04_29T14_58_46_mrs_ImageRecipes_145026lrg_1080_850.jpg",
+    "precio": 3700
+  }
+]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from main.pasteles import pasteles
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {'productos': pasteles}
+    return render(request, 'index.html', context)
 
 def about(request):
     return render(request, 'about.html')
