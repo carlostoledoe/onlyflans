470d060b2668f1576e4fd67714b2c956996fd55d
Carlos Toledo <carlombo@hotmail.com>
2024-06-07 19:29:05 -0400

    [STYLE] Updated styles of the form

--- main/forms.py
+++ main/forms.py
@@ -5,21 +5,21 @@ class FlanForm(forms.Form):
         max_length=10,
         label='Nombre:',
         widget=forms.TextInput(attrs={
-            'class': 'form-control'
+            'class': 'form-control mb-3'
         })
     )
     email = forms.EmailField(
-        max_length=10,
+        max_length=20,
         label='Email:',
         widget=forms.EmailInput(attrs={
-            'class': 'form-control',
+            'class': 'form-control mb-3',
             'placeholder': 'Ingrese correo aquí'
         })
     )
     mensaje = forms.CharField(
         label='Mensaje:',
         widget=forms.Textarea(attrs={
-            'class': 'form-control',
-            'rows': 6,
+            'class': 'form-control mb-3',
+            'rows': 5,
         })
     )
