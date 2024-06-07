b76be790b011f201b72af3c7b8a2502d31aeef33
Carlos Toledo <carlombo@hotmail.com>
2024-06-07 18:33:00 -0400

    [ADD] Created Flan model and makemigrations and migrate

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Flan(models.Model):
+    flan_uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    name = models.CharField(
+        max_length=64
+    )
+    description = models.TextField()
+    image_url = models.URLField()
+    slug = models.SlugField()
+    is_private = models.BooleanField()
