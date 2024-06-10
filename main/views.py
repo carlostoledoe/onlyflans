2b9cedd34a9038d16c3eaa260293f72d344086ce
Carlos Toledo <carlombo@hotmail.com>
2024-06-10 16:15:02 -0400

    [UPDATE] Updated views and index template to show flans from SQLite database

--- main/views.py
+++ main/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render, redirect
 from main.forms import ContactForm
-from main.models import Contact
+from main.models import Contact, Flan
 
 import json
 import os
 
 # Create your views here.
 def index(request):
-    # file_path = 'main/data/pasteles.json'
-    file_path = os.path.join(os.path.dirname(__file__), 'data', 'pasteles.json') # Obtiene el directorio de views.py y combina con 'data' y 'personas.json' para crear la ruta. Esta forma es portable, mantenible y buena práctica
-    with open(file_path, 'r') as file: 
-        pasteles = json.load(file)
-    context = {'productos': pasteles}
+    flanes = Flan.objects.all()
+    context = {'flanes': flanes}
     return render(request, 'index.html', context)
 
 def about(request):
