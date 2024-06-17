d8437dc1012acde08418f23540ba48a61f109741
Carlos Toledo <carlombo@hotmail.com>
2024-06-17 19:03:29 -0400

    [CHORE] Cleaning code

--- main/data/api_mindicador.py
+++ main/data/api_mindicador.py
@@ -7,9 +7,3 @@ def request_get(url):
     return json.loads(requests.get(url).text) 
 
 indicators = request_get(url)
-
-valor_dolar = indicators['dolar']['valor']
-valor_euro = indicators['euro']['valor']
-valor_uf = indicators['uf']['valor']
-valor_utm = indicators['utm']['valor']
-valor_bitcoin = indicators['bitcoin']['valor']
