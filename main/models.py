9c9b3dcd60276f530b447e5de9cdd19899f27e9f
Carlos Toledo <carlombo@hotmail.com>
2024-06-18 22:40:38 -0400

    [ADD] Added function in the model to assign 'diamond' user type to newly created admin

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import uuid
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -43,3 +45,8 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and instance.is_superuser:
+        UserProfile.objects.create(user=instance, user_type='diamond')
